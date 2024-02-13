628af3a611f4feee350dbf3ea42de3a66caeba62
Costi <costy_17_rdv@yahoo.com>
2024-02-13 14:05:09 +0000

    enhancement: timestamps

---
+++ src/books/model.js
@@ -0,0 +1,23 @@
+const { DataTypes } = require("sequelize");
+
+const sequelize = require("../db/connection");
+
+const Book = sequelize.define("Book", {
+  title: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  author: {
+    type: DataTypes.STRING,
+    defaultValue: "sameauthor",
+  },
+  genre: {
+    type: DataTypes.STRING,
+    defaultValues: "same genre",
+  },
+}
+ ( timestamps : false )
+);
+
+module.exports = Book;
